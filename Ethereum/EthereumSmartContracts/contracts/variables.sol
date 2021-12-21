b88af95ac56bf4a48b236a117c2f1d5362bb7c68
Prabesh <prabesh.bista369@gmail.com>
2021-12-21 14:19:27 +0545

    variables,struct

---
+++ Ethereum/EthereumSmartContracts/contracts/variables.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract MyContract{
+    //state variable
+
+    //Local Variable
+    function getValue()public pure returns(uint){
+        uint value=1;
+        return value;
+    }
+}
