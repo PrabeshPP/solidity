8386181326b7c424dfec968c45c57165ac368ebc
Prabesh <prabesh.bista369@gmail.com>
2021-12-21 13:35:48 +0545

    count++

--- Ethereum/EthereumSmartContracts/contracts/simpleStorage.sol
+++ Ethereum/EthereumSmartContracts/contracts/simpleStorage.sol
@@ -3,16 +3,9 @@ pragma solidity ^0.8.0;
 
 contract Counter{
 // uint means unsigned integer
-    uint count;
-
-    constructor() {
-            count=0;
-    }
-    function getCount()public view returns(uint){
-        return count;
-    }
+    uint public count=0;
 
     function incrementCount()public {
-        count=count+1;
+        count++;
     }
 }
