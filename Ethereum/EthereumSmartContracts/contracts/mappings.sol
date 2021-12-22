5c08acb69c5168f4809df5122755978e9e4d3cf6
Prabesh <prabesh.bista369@gmail.com>
2021-12-22 15:03:58 +0545

    mapping

--- Ethereum/EthereumSmartContracts/contracts/mappings.sol
+++ Ethereum/EthereumSmartContracts/contracts/mappings.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.1;
 
 contract MapContract{
-    
+mapping(uint=>string) public names;
+constructor(){
+    names[1]="Adam";
+    names[2]="Prabesh";
+    names[3]="Abhinav";
+    names[4]="Carlo";
+}
+
+
+
 }
