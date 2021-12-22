3f003146432556faf67cf8c645cf47030620f302
Prabesh <prabesh.bista369@gmail.com>
2021-12-22 15:05:53 +0545

    struct,mapping

--- Ethereum/EthereumSmartContracts/contracts/mappings.sol
+++ Ethereum/EthereumSmartContracts/contracts/mappings.sol
@@ -3,11 +3,21 @@ pragma solidity ^0.8.1;
 
 contract MapContract{
 mapping(uint=>string) public names;
+mapping(uint=>Books) public books;
+
+struct Books{
+    string title;
+    string author;
+}
 constructor(){
     names[1]="Adam";
     names[2]="Prabesh";
     names[3]="Abhinav";
     names[4]="Carlo";
+}
+
+function addBooks(uint id,string memory _title,string memory _author)public{
+        books[id]=Books(_title,_author);
 }
 
 
