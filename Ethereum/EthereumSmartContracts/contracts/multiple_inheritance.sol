482bd9e6e1880f4dab7f78a6731382cf9fec6313
Prabesh <prabesh.bista369@gmail.com>
2021-12-22 18:07:32 +0545

    multiple_inheritance

---
+++ Ethereum/EthereumSmartContracts/contracts/multiple_inheritance.sol
@@ -0,0 +1,46 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+contract Ownable{
+    address owner;
+    constructor(){
+        owner=msg.sender;
+    }
+    modifier onlyOwner(){
+    require(msg.sender==owner,"you must be owner");
+    _;
+
+}
+}
+
+contract SecrectVault{
+    string secrect;
+
+    constructor(string memory _secrect){
+        secrect=_secrect;
+
+    }
+
+    function getSecrect() public view returns(string memory){
+        return secrect;
+    }
+}
+
+contract Mycontract is Ownable{
+    string secrect;
+    address secrectVault;
+    constructor(string memory _secrect){
+     SecrectVault _secrectVault=new SecrectVault(_secrect);
+    secrectVault=address(_secrectVault);
+
+        super;
+    }
+
+    function getSecrect()public view onlyOwner returns(string memory){
+        SecrectVault _secrectVault=SecrectVault(secrectVault);
+        return _secrectVault.getSecrect();
+
+    }
+
+
+}
