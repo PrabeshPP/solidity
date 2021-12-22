b0501c71d77413e6a7144720cf097a7f698dc8c1
Prabesh <prabesh.bista369@gmail.com>
2021-12-22 15:41:43 +0545

    if_else,for

--- Ethereum/EthereumSmartContracts/contracts/if_else.sol
+++ Ethereum/EthereumSmartContracts/contracts/if_else.sol
@@ -2,6 +2,25 @@
 pragma solidity ^0.8.1;
  contract MyContract1{
 
+uint[] public numbers=[1,2,3,4,5,6,7,8,9,10];
+
+address public owner;
+constructor(){
+    owner=msg.sender;
+}
+
+function countEvenNumbers() public view returns(uint){
+    uint count=0;
+    // for loop
+    for(uint i=0;i<numbers.length;i++){
+        if(isEvenNumber(numbers[i])){
+            count++;
+        }
+
+    }
+
+    return count;
+}
 
     function isEvenNumber(uint _number)public pure returns(bool) {
          if(_number % 2==0){
@@ -10,5 +29,14 @@ pragma solidity ^0.8.1;
              return false;
          }
      
+    }
+    //checking if the deployer is owner or not for the contracts
+    function isOwner() public view returns(bool){
+        if(msg.sender==owner){
+            return true;
+
+        }else{
+            return false;
+        }
     }
  }
