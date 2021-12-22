f19e7d4372ccee61d092d6fffa0dbfd5f0231474
Prabesh <prabesh.bista369@gmail.com>
2021-12-22 15:31:19 +0545

    if_else

--- Ethereum/EthereumSmartContracts/contracts/if_else.sol
+++ Ethereum/EthereumSmartContracts/contracts/if_else.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
+ contract MyContract1{
 
-contract MyContract{
-    
-}
+
+    function isEvenNumber(uint _number)public pure returns(bool) {
+         if(_number % 2==0){
+             return true;
+         }else{
+             return false;
+         }
+     
+    }
+ }
