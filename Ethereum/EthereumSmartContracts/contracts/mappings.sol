2cdfa2e79a2c73ba0a2c28295c682cc05b002ae8
Prabesh <prabesh.bista369@gmail.com>
2021-12-22 15:17:32 +0545

    nested mapping

--- Ethereum/EthereumSmartContracts/contracts/mappings.sol
+++ Ethereum/EthereumSmartContracts/contracts/mappings.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.1;
 contract MapContract{
 mapping(uint=>string) public names;
 mapping(uint=>Books) public books;
+//nested mapping
+mapping(address=>mapping(uint=>Books)) public myBooks;
 
 struct Books{
     string title;
@@ -20,6 +22,9 @@ function addBooks(uint id,string memory _title,string memory _author)public{
         books[id]=Books(_title,_author);
 }
 
+function addMyBooks(uint _id,string memory _title,string memory _author) public{
+    myBooks[msg.sender][_id]=Books(_title,_author);
+}
 
 
 }
