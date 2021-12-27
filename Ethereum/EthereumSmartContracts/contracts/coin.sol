2f5bf71a70abddd73c4e461e40fb009be33bd5f4
Prabesh <prabesh.bista369@gmail.com>
2021-12-27 10:15:36 +0545

    coin solidity

--- Ethereum/EthereumSmartContracts/contracts/coin.sol
+++ Ethereum/EthereumSmartContracts/contracts/coin.sol
@@ -2,5 +2,40 @@
 pragma solidity  ^0.8.1;
 
 contract Coin{
+    address public minter; // declare a state variable of type address
+    mapping(address=>uint) public balances;
+
+    //constructor only runs one ,so we don't require public 
+    constructor(){
+        minter=msg.sender;
+    }
+    event Sent(address from,address to,uint amount); // it emits the state when the sent event is called 
     
+    // this function would allow us to create coin and only the owner of the contract can create the coin
+    function mint(address receiver,uint amount)public{
+        require(msg.sender==minter);
+        balances[receiver]+=amount;
+
+
+    }
+
+    //it wil return error 
+    error InsufficientBalance(uint requested,uint available);
+
+    //this function would allow us to send the created coin to the receiver address
+    function send(address receiver,uint amount)public{
+        if(amount>balances[msg.sender])
+        revert InsufficientBalance({
+            requested:amount,
+            available:balances[msg.sender]
+        });
+
+        balances[msg.sender]-=amount;
+        balances[receiver]+=amount;
+        emit Sent(msg.sender, receiver, amount);
+
+    }
+
+
+
 }
