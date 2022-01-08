9bafd6df1cc62fe52debf487f3005d43d8b43350
Prabesh <prabesh.bista369@gmail.com>
2022-01-08 09:17:49 +0545

    smart contracts for voting

--- Ethereum/EthereumSmartContracts/contracts/vote.sol
+++ Ethereum/EthereumSmartContracts/contracts/vote.sol
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
+
+contract Ballot{
+
+    //the owner of this smart contract is the chairperson for this voting system
+address public chairperson;
+
+struct Voter{
+    uint weight;
+    bool voted;
+    address delegate;
+    uint vote;
+}
+
+struct Proposal{
+    bytes32 name;
+    uint voteCount;
+}
+
+}
